bcb021a2a2124b352a7f48041c4fe7c13c7ae65b
dimka11 <dimka11lp@gmail.com>
2017-06-04 00:37:39 +0600

    Just added form

---
+++ EditClient.pas
@@ -0,0 +1,24 @@
+unit EditClient;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TEditClientForm = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  EditClientForm: TEditClientForm;
+
+implementation
+
+{$R *.dfm}
+
+end.
